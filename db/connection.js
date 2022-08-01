126685f60f7291ed44ce91a2f7ae1970017ae598
Brian <73gs514@gmail.com>
2022-08-01 12:39:28 -0400

    db update

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,17 @@
-const mysql = require("mysql2");
+// Require module
+const mysql = require('mysql2');
 
+// Connect to db
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "tracker_db"
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'employee_db',
 });
 
-connection.connect(function (err) {
-  if (err) throw err;
+connection.connect((err) => {
+    if (err) throw err;
 });
 
+// Export connection
 module.exports = connection;
